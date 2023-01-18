d8ca50bd52e9df2c7765e7131c5f4090216fc5b9
Yacob Kulandaisamy <Yacob.Kulandaisamy@rencata.com>
2023-01-18 02:58:17 -0800

    initial commit on the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentRegistrationComponent } from './components/student-registration/student-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentRegistrationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/components.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { StudentRegistrationComponent } from '../components/student-registration/student-registration.component';
+
+@NgModule({
+  declarations: [
+    StudentRegistrationComponent
+  ],
+  imports: [
+    BrowserModule,
+  ],
+  providers: [],
+})
+export class ComponentModule { }
